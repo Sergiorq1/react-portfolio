86d84a86d424c58d53df1a9ef1f5054bb24c0c11
Sergiorq1 <66581064+Sergiorq1@users.noreply.github.com>
2022-02-07 17:18:04 -0700

    fixed cards styles

--- my-app/src/components/Cards.js
+++ my-app/src/components/Cards.js
@@ -10,22 +10,36 @@ function Cards() {
                 <ul className="cards-items">
                     <CardItem
                     src='https://api.lorem.space/image?'
-                    text='Explore The horizons'
-                    label="adventure"
+                    text='NFT Puzzle Game'
+                    label="Blockchain"
                     path='/services'
                     />
                     <CardItem
                     src='https://api.lorem.space/image/car?'
-                    text='Explore The horizons'
-                    label="adventure"
+                    text='Fun facts'
+                    label="API"
+                    path='/services'
+                    />
+                </ul>
+                <ul className="cards-items">
+                    <CardItem
+                    src='https://api.lorem.space/image/fashion?'
+                    text='Spotify playlist'
+                    label="music"
                     path='/services'
                     />
                     <CardItem
-                    src='https://api.lorem.space/image/pizza?'
-                    text='Explore The horizons'
-                    label="adventure"
+                    src='https://api.lorem.space/image/shoes?'
+                    text='Basketball Visualization'
+                    label="music"
                     path='/services'
                     />
+                    <CardItem
+                    src='https://api.lorem.space/image/watch?'
+                    text='Spotify playlist'
+                    label="music"
+                    path='/services'
+                    /> 
                 </ul>
             </div>
         </div>
