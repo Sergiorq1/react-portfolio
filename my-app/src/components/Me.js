7f05ebd0dae2782055d9981eedea861c1262d4f1
Sergiorq1 <66581064+Sergiorq1@users.noreply.github.com>
2022-02-13 10:29:03 -0700

    me.js page styling more responsive

--- my-app/src/components/Me.js
+++ my-app/src/components/Me.js
@@ -6,9 +6,9 @@ function Me() {
   return (
     <div className="me-container">
         <h1>About Me:</h1>
-        <p> I'm a 19-year-old 2nd year student at Dominican University of California studying back-end web development.</p>
-        <p>I have experience working with front-end frameworks like react and bootstrap, and I am comfortable with back-end stack like node.js, mongoDB, AWS Lambda, and Docker. My favorite thing about being a software engineer is the continuous learning of new technology and collaboration with people who are just as passionate as I am.</p>
-        <p>Outside of tech, growing up with many musicians in my family has inspired my love for regional mexican music and jazz. Another important aspect of my upbringing is nature, since I was raised in central california away from big cities and near national parks like Yosemite, Kings Canyon, and Sequoia. Places like the Underground Garden and Woodward Park in Fresno are often my mental go-to's when meditating because of the "harmonic tranquility" they represent in my memories.</p>
+        <p> I'm a 19-year-old 2nd year student at Dominican University of California studying back-end web development. <hr/>
+        I have experience working with front-end frameworks like react and bootstrap, and I am comfortable with back-end stack like node.js, mongoDB, AWS Lambda, and Docker. My favorite thing about being a software engineer is the continuous learning of new technology and collaboration with people who are just as passionate as I am.<hr/>
+        Outside of tech, growing up with many musicians in my family has inspired my love for regional mexican music and jazz. Another important aspect of my upbringing is nature, since I was raised in central california away from big cities and near national parks like Yosemite, Kings Canyon, and Sequoia. Places like the Underground Garden and Woodward Park in Fresno are often my mental go-to's when meditating because of the "harmonic tranquility" they represent in my memories.</p>
     </div>
   )
 }
