bb468f88561aff75371fca4f9282f3a20fe87ec7
Sergiorq1 <66581064+Sergiorq1@users.noreply.github.com>
2022-07-20 01:26:04 -0600

    Updates

--- README.md
+++
@@ -1,8 +0,0 @@
-# react-portfolio
-My website showcasing several things about be, including what I do, projects I've worked on, and interesting facts about me
-
-## Tech Stack
-
-**Client:** React, Redux, TailwindCSS, Bootsrap
-
-**Server:** Node, Express
---
+++ precache-manifest.521c5e8458b64ed94d68aadc2e946573.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f20b29b56df34d2cf2f",
+    "url": "/react-portfolio/static/css/main.57b3e77a.chunk.css"
+  },
+  {
+    "revision": "8f20b29b56df34d2cf2f",
+    "url": "/react-portfolio/static/js/main.8f20b29b.chunk.js"
+  },
+  {
+    "revision": "c28598df6b05e69db359",
+    "url": "/react-portfolio/static/js/1.c28598df.chunk.js"
+  },
+  {
+    "revision": "f8926043fa4459ec0101",
+    "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
+  },
+  {
+    "revision": "9fedc502b9e8e46b2d1416006ea77fb8",
+    "url": "/react-portfolio/static/media/space-galaxy-background.9fedc502.jpg"
+  },
+  {
+    "revision": "9b81f8439712353458f7a2aeb20aa539",
+    "url": "/react-portfolio/static/media/carphoto.9b81f843.png"
+  },
+  {
+    "revision": "1a25cfe052b2943d16eae2a3918c0f95",
+    "url": "/react-portfolio/index.html"
+  }
+];
